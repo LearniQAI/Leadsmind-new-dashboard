63a634df0af0edcc9c9656f7558293d7d6f653b1
Zain Ul Hassan <zainulhassan5857@gmail.com>
2026-07-02 09:19:11 +0500

    Refactor Supabase client creation logic

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,13 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
- throw new Error('[FATAL] Supabase env vars are not set')
-}
-
 export const createClient = () => {
- return createBrowserClient(supabaseUrl, supabaseAnonKey)
-}
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('[FATAL] Supabase env vars are not set')
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseAnonKey)
+}
